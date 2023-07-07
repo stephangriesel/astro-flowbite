0127018e158247a941de560f2a2c5116da2b8bad
Stephan Griesel <sgriesel@gmail.com>
2023-07-07 06:30:08 +0200

    clean & update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const SITE = {
-  title: 'Hlokomela - Award winning HIV and AIDS educational and treatment programme in Limpopo and Mpumalanga South Africa',
+  title: 'Astro TailWind Flowbite',
 };
